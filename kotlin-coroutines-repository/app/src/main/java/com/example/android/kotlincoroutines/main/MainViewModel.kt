19bec097a704b0375b262500629108b702ab4d33
linucksrox <ericdalyblog@dalydays.com>
2019-02-01 06:39:09 -0500

    Complete part 10, break out refresh logic with higher order function

--- kotlin-coroutines-repository/app/src/main/java/com/example/android/kotlincoroutines/main/MainViewModel.kt
+++ kotlin-coroutines-repository/app/src/main/java/com/example/android/kotlincoroutines/main/MainViewModel.kt
@@ -124,17 +124,9 @@ class MainViewModel(private val repository: TitleRepository) : ViewModel() {
     /**
      * Refresh the title, showing a loading spinner while it refreshes and errors via snackbar.
      */
-    // TODO: Change this implementation to use coroutines
     fun refreshTitle() {
-        uiScope.launch {
-            try {
-                _spinner.value = true
-                repository.refreshTitle()
-            } catch (error: TitleRepository.TitleRefreshError) {
-                _snackBar.value = error.message
-            } finally {
-                _spinner.value = false
-            }
+        launchDataLoad {
+            repository.refreshTitle()
         }
     }
 
@@ -149,5 +141,16 @@ class MainViewModel(private val repository: TitleRepository) : ViewModel() {
      *              lambda the loading spinner will display, after completion or error the loading
      *              spinner will stop
      */
-    // TODO: Add launchDataLoad here then refactor refreshTitle to use it
+    private fun launchDataLoad(block: suspend () -> Unit): Job {
+        return uiScope.launch {
+            try {
+                _spinner.value = true
+                block()
+            } catch (error: TitleRepository.TitleRefreshError) {
+                _snackBar.value = error.message
+            } finally {
+                _spinner.value = false
+            }
+        }
+    }
 }
